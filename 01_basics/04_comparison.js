9feeaa1d3ed27cae4a96af3d5dc063852f6e131f
aaratirathavade <35604929+aaratirathavade@users.noreply.github.com>
2025-09-23 17:11:17 +0000

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,16 @@
+//console.log("3" > 5)
+//console.log(null > 0) // false
+//console.log(null < 0) //false
+//console.log(null == 0) //false
+//console.log(null >= 0) // true
+
+
+//console.log(undefined > 0) //false
+//console.log(undefined < 0) //false
+//console.log(undefined >= 0) //false
+//console.log(undefined == 0) //false
+
+
+// strict check === (value + data type)
+console.log("2" == 2) // true 
+console.log("2" === 2) // false
